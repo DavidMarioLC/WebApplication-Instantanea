16f514a322e47d6f9e1933c7801a63fd767ed24f
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-08-04 00:15:54 -0500

    add opengraph

--- js/story.js
+++ js/story.js
@@ -2,11 +2,11 @@ const $btnModalNext = document.querySelector('.button.modalStory__button.next');
 const $modalStory = document.querySelector('.modalStory');
 const $btnModalClose = document.querySelector('#btnModalClose');
 
-const $myStory = document.querySelector('#myStory');
+const $myStoryList = document.querySelectorAll('.story-pick');
 const $video = document.querySelector('.modalStory__video');
 
-// $modalStory.showModal();
 const $progressVideo = document.querySelector('.modalStory__progress__item');
+
 
 $progressVideo.max = $video.duration;
 
@@ -45,7 +45,7 @@ function updateProgress(){
 }
 
 $video.addEventListener('timeupdate',updateProgress);
-$myStory.addEventListener('click',showStorie);
+$myStoryList.forEach(story=>story.addEventListener('click',showStorie));
 
 $btnModalClose.addEventListener('click',closeStorie);
 $btnModalNext.addEventListener('click',nextHistory);
